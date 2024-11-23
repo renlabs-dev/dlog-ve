d411fed721ef1cb29644a8ddb2296a47febe9853
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2024-11-23 10:22:11 -0300

    fix: fix stuff

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,7 @@ use centipede::juggling::{
     proof_system::{Helgamalsegmented, Proof, Witness},
     segmentation::Msegmentation,
 };
-use curv::{
-    arithmetic::{Converter, Modulo},
-    elliptic::curves::{Point, Scalar, Secp256k1},
-    BigInt,
-};
+use curv::elliptic::curves::{Point, Scalar, Secp256k1};
 
 pub const SEGMENT_SIZE: usize = 8;
 pub const NUM_OF_SEGMENTS: usize = 32;
@@ -20,7 +16,6 @@ pub enum EncryptError {
     MismatchedPointOrder(curv::elliptic::curves::MismatchedPointOrder),
 }
 
-use rand::Rng;
 pub use EncryptError::*;
 
 pub fn encrypt(
@@ -69,65 +64,46 @@ pub fn verify(
     let g = Point::generator();
     proof
         .verify(encryptions, &g, &encryption_key, &public_key, &SEGMENT_SIZE)
-        .inspect_err(|err| {
-            dbg!(err);
-        })
         .is_ok()
-}
-
-pub fn gen_key_pair<R: Rng>(rng: &mut R) -> (Point<Secp256k1>, Scalar<Secp256k1>) {
-    let mut bytes = [0u8; 32];
-    rng.fill_bytes(&mut bytes);
-    (gen_random_point(&bytes), Scalar::random())
-}
-
-fn gen_random_point(bytes: &[u8]) -> Point<Secp256k1> {
-    let compressed_point_len = secp256k1::constants::PUBLIC_KEY_SIZE;
-    let truncated = if bytes.len() > compressed_point_len - 1 {
-        &bytes[0..compressed_point_len - 1]
-    } else {
-        bytes
-    };
-    let mut buffer = [0u8; secp256k1::constants::PUBLIC_KEY_SIZE];
-    buffer[0] = 0x2;
-    buffer[1..1 + truncated.len()].copy_from_slice(truncated);
-    if let Ok(point) = Point::from_bytes(&buffer) {
-        return point;
-    }
-
-    let bn = BigInt::from_bytes(bytes);
-    let two = BigInt::from(2);
-    let bn_times_two = BigInt::mod_mul(&bn, &two, Scalar::<Secp256k1>::group_order());
-    let bytes = BigInt::to_bytes(&bn_times_two);
-    gen_random_point(&bytes)
 }
 
 #[cfg(test)]
 mod tests {
-    use rand::thread_rng;
+    use centipede::wallet::SecretShare;
 
     use super::*;
 
     #[test]
     fn probabilistic_test() {
-        let (encryption_key, _decryption_key) = gen_key_pair(&mut thread_rng());
-        let (_public_key, secret_key) = gen_key_pair(&mut thread_rng());
+        let enc = SecretShare::generate();
+        let dlog = SecretShare::generate();
 
-        let a =
-            serde_json::to_string(&encrypt(encryption_key.clone(), secret_key.clone()).unwrap())
-                .unwrap();
-        let b = serde_json::to_string(&encrypt(encryption_key, secret_key).unwrap()).unwrap();
+        let a = serde_json::to_string(&encrypt(enc.pubkey.clone(), dlog.secret.clone()).unwrap())
+            .unwrap();
+        let b = serde_json::to_string(&encrypt(enc.pubkey, dlog.secret).unwrap()).unwrap();
         assert_ne!(a, b);
     }
 
     #[test]
     fn zk_test() {
-        let (encryption_key, _decryption_key) = gen_key_pair(&mut thread_rng());
-        let (public_key, secret_key) = gen_key_pair(&mut thread_rng());
-        let (witness, ciphertexts) = encrypt(encryption_key.clone(), secret_key.clone()).unwrap();
-        let proof = prove(encryption_key.clone(), witness, &ciphertexts).unwrap();
-        let verified = verify(proof, encryption_key, public_key, &ciphertexts);
+        let enc = SecretShare::generate();
+        let dlog = SecretShare::generate();
+        let (witness, ciphertexts) = encrypt(enc.pubkey.clone(), dlog.secret.clone()).unwrap();
+        let proof = prove(enc.pubkey.clone(), witness, &ciphertexts).unwrap();
+        let verified = verify(proof, enc.pubkey, dlog.pubkey, &ciphertexts);
 
         assert!(dbg!(verified));
+    }
+
+    #[test]
+    fn zk_wrong_test_1() {
+        let enc = SecretShare::generate();
+        let dlog = SecretShare::generate();
+        let random_dlog = SecretShare::generate();
+        let (witness, ciphertexts) = encrypt(enc.pubkey.clone(), dlog.secret.clone()).unwrap();
+        let proof = prove(enc.pubkey.clone(), witness, &ciphertexts).unwrap();
+        let verified = verify(proof, random_dlog.pubkey, dlog.pubkey, &ciphertexts);
+
+        assert!(!verified);
     }
 }
